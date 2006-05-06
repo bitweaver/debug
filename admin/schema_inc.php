f7cf716babce02316330f19e7ab2571cc07420e4
Max Kremmel <xing@synapse.plus.com>
2006-05-06 15:48:07 +0000

    clean up schema files, remove unused PackageInfo data - we really need a package manager for this, add plugin_path column to liberty_plugins

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -2,8 +2,5 @@
 $gBitInstaller->registerPackageInfo( DEBUG_PKG_NAME, array(
 	'description' => "DEBUG allows general debugging of Bitweaver.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
-	'version' => '0.1',
-	'state' => 'beta',
-	'dependencies' => '',
 ) );
 ?>
