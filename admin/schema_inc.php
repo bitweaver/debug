b9b7fc7c183803d224ba269917368ead95dde409
Max Kremmel <xing@synapse.plus.com>
2006-07-23 08:04:50 +0000

    update globals in schema files

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -1,4 +1,5 @@
 <?php
+global $gBitInstaller;
 $gBitInstaller->registerPackageInfo( DEBUG_PKG_NAME, array(
 	'description' => "DEBUG allows general debugging of Bitweaver.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
