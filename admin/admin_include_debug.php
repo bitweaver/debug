bca84601b7a303b9d9c2798961025a05036f9502
wjames5 <will@tekimaki.com>
2009-10-01 14:16:59 +0000

    remove ref to non-existant copyright.txt file

--- admin/admin_include_debug.php
+++ admin/admin_include_debug.php
@@ -1,9 +1,9 @@
 <?php
 
-// $Header: /cvsroot/bitweaver/_bit_debug/admin/admin_include_debug.php,v 1.2 2009/10/01 13:45:34 wjames5 Exp $
+// $Header: /cvsroot/bitweaver/_bit_debug/admin/admin_include_debug.php,v 1.3 2009/10/01 14:16:59 wjames5 Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// All Rights Reserved. See below for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 $features_toggles = array(
 	"feature_debug_console",
